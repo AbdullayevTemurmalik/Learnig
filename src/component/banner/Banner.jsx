dd08e359f9a41c5b32740b377455493d109a4ace
AbdullayevTemurmalik <tmaq77@gamil,com>
2025-10-20 20:16:50 +0500

    Learning Redux Toolkit

--- src/component/banner/Banner.jsx
+++ src/component/banner/Banner.jsx
@@ -1,10 +1,13 @@
-import React from "react";
-import "./Banner.css"
+import "./Banner.css";
+import { useSelector } from "react-redux";
 
 const Banner = () => {
+  const count = useSelector((state) => state.counter.value);
+
   return (
-    <div>
+    <div className="banner">
       <h1>Banner</h1>
+      <h2>Counter: {count}</h2>
     </div>
   );
 };
--- src/component/navbar/Navbar.jsx
+++ src/component/navbar/Navbar.jsx
@@ -1,10 +1,19 @@
 import React from "react";
-import "./Navbar.css"
+import "./Navbar.css";
+import { useDispatch } from "react-redux";
+import { increment, decrement, reset } from "../../features/counterSlice";
 
 const Navbar = () => {
+  const dispatch = useDispatch();
+
   return (
-    <div>
+    <div className="navbar">
       <h1>Navbar</h1>
+      <div className="buttons">
+        <button onClick={() => dispatch(increment())}>+</button>
+        <button onClick={() => dispatch(decrement())}>-</button>
+        <button onClick={() => dispatch(reset())}>Reset</button>
+      </div>
     </div>
   );
 };

831a57d0e50fa3e60cacccd18664a40e0473611e
AbdullayevTemurmalik <tmaq77@gamil,com>
2025-10-20 19:58:36 +0500

    Learning Redux Toolkit

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
-import './App.css'
+import "./App.css";
+import Banner from "./component/banner/Banner";
+import Navbar from "./component/navbar/Navbar";
 
 function App() {
- 
-
   return (
     <>
-      
+      <Navbar />
+      <Banner />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
